9d5112f3b23651c171f56acf8aa6cf2d0a0b0755
Paulina LH <65806675+paulinalh@users.noreply.github.com>
2022-05-31 22:52:44 -0500

    Actualice archivo

--- act5_3.erl
+++ act5_3.erl
@@ -1,5 +1,7 @@
 -module(act5_3).
 -export([lee/1]).
+-import(lists,[nth/2]). 
+-import(string, [slice/3]).
 
 lee(File) ->
     FileIn = File ++ "1.in",
@@ -9,5 +11,16 @@ lee(File) ->
 
     List1 = string:split(Txt, " ", all),
     List1,
+    %convertir a int
+    PrimerInt = list_to_integer(nth(1,List1)),
+    Operador = nth(2,List1),
+    %SegundoInt = list_to_integer(nth(3,List1)),
+    SegundoInt = list_to_integer(slice([nth(3,List1)], -2,2)),
+    
+    if
+        Operador == "+" -> PrimerInt + SegundoInt
+    end,
+    
     {ok, SOut} = file:open(FileOut, [write]),
     io:format(SOut, "Hola Crayola!~s~n", [List1]).
+
