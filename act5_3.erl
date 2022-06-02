85a2df18506e139142f8acdfc2bf4366e817a28c
panaze <panaze@outlook.com>
2022-06-02 10:39:43 -0500

    Imprime primera linea con la operación hecha en archivo.out

--- act5_3.erl
+++ act5_3.erl
@@ -2,25 +2,34 @@
 -export([lee/1]).
 -import(lists,[nth/2]). 
 -import(string, [slice/3]).
+-import(string, [len/1]).
 
 lee(File) ->
+
     FileIn = File ++ "1.in",
     FileOut = File ++ "1.out",
     {ok, SIn} = file:open(FileIn, read),
     Txt = io:get_line(SIn, ''),
 
     List1 = string:split(Txt, " ", all),
-    List1,
+
     %convertir a int
     PrimerInt = list_to_integer(nth(1,List1)),
+
     Operador = nth(2,List1),
-    %SegundoInt = list_to_integer(nth(3,List1)),
-    SegundoInt = list_to_integer(slice([nth(3,List1)], -2,2)),
+
+    Length = len(nth(3,List1)),
+    SegundoInt = list_to_integer(string:slice([nth(3,List1)], 0, Length-1)),
     
-    if
-        Operador == "+" -> PrimerInt + SegundoInt
+    case  Operador of 
+        "+" -> Respuesta = PrimerInt + SegundoInt; 
+        "-" -> Respuesta = PrimerInt - SegundoInt; 
+        "*" -> Respuesta = PrimerInt * SegundoInt; 
+        "/" -> Respuesta = PrimerInt / SegundoInt; 
+        "%" -> Respuesta = PrimerInt rem SegundoInt
     end,
     
     {ok, SOut} = file:open(FileOut, [write]),
-    io:format(SOut, "Hola Crayola!~s~n", [List1]).
+    io:format(SOut,"~p~n", [Respuesta]).
+    
 
