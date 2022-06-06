a5247261cccebd1392008c9a5925cef5e3c51f9a
Paulina LH <65806675+paulinalh@users.noreply.github.com>
2022-06-06 10:39:56 -0500

    Update parte secuencial terminada

--- act5_3.erl
+++ act5_3.erl
@@ -1,26 +1,31 @@
 -module(act5_3).
+-export([run/0]).
 -export([lee/1]).
--export([operacion/2]).
--export([imprimir/3]).
+-export([operacion/3]).
+-export([imprimir/4]).
 -import(lists,[nth/2]). 
 -import(string, [slice/3]).
 -import(string, [len/1]).
 
-lee(File) ->
+run() ->
+    NumArchivo =1,
+    lee(NumArchivo).
 
-    %Guardamos nombres del archivo de entrada y salida
-    FileIn = File ++ "1.in", 
-    FileOut = File ++ "1.out",
-    file:delete(File ++ "1.out"),
-    {ok, SIn} = file:open(FileIn, read), %Abrimos archivo de entrada,
-    {ok, SOut} = file:open(FileOut, [append]),
+lee(NumArchivo) ->
+    if 
+        NumArchivo >4 -> io:format("");
+        true -> FileIn = "case" ++ integer_to_list(NumArchivo)++".in", %Guardamos nombres del archivo de entrada y salida
+            FileOut = "case" ++ integer_to_list(NumArchivo)++".out",
+            file:delete(FileOut),
+            {ok, SIn} = file:open(FileIn, read), %Abrimos archivo de entrada,
+            {ok, SOut} = file:open(FileOut, [append]),
+            operacion(SIn, SOut, NumArchivo) 
+    end.
 
-    operacion(SIn, SOut).
-
-operacion(SIn,SOut) -> 
+operacion(SIn,SOut, NumArchivo) -> 
     Txt = io:get_line(SIn, ''),
     if 
-        Txt == eof -> file:close(SIn);
+        Txt == eof -> file:close(SIn), file:close(SOut), lee(NumArchivo +1);
         true ->  %Obtenemos primera linea del archivo
 
             List1 = string:split(Txt, " ", all), %Convertimos cada elemento de la linea en una misma lista
@@ -43,14 +48,14 @@ operacion(SIn,SOut) ->
                 "%" -> Respuesta = PrimerInt rem SegundoInt
             end,
 
-            imprimir(SIn, Respuesta,SOut)
+            imprimir(SIn, Respuesta,SOut, NumArchivo)
     end.
     
    
 
 %Imprimimos respuesta en archivo de salida
-imprimir(SIn, Respuesta,SOut) ->
+imprimir(SIn, Respuesta,SOut, NumArchivo) ->
     io:format(SOut,"~s~n", [integer_to_list(Respuesta)]),
-    operacion(SIn, SOut).
+    operacion(SIn, SOut, NumArchivo).
     
 
